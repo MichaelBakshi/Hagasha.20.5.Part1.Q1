272dee619d5cb76cc0db348e7ffabc5d09004561
MichaelBakshi <62958898+MichaelBakshi@users.noreply.github.com>
2020-06-10 18:07:38 +0300

    Add files via upload

---
+++ Hagasha.20.5.Part1.Q1/Hagasha.20.5.Part1.Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagasha._20._5.Part1.Q1
+{
+    class Program
+    {
+
+        private static void Power2(ref int i)
+        {
+            i = i * i;
+
+
+        }
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine("Enter a number:");
+             x = Convert.ToInt32(Console.ReadLine());
+            Power2(ref x);
+            Console.WriteLine($"power 2 of your number is {x}");
+            Console.ReadLine();
+        }
+       
+
+    }
+}
